4b0ab86186595de62af1687a13841e6f65713056
unknown <mvaluh@gmail.com>
2021-10-26 18:47:21 +0200

    About Page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,10 +7,10 @@ export default function About() {
   return (
     <div className="container">
       <div className="about_content">
-        <h3 className="about_text">
-          I’m a Front-End developer who cares deeply user experience. Striving
-          for simplicity, elegance and a great challenge!
-        </h3>
+        <h4 className="about_text">
+          I’m a Front-End developer who cares deeply about user experience.
+          Striving for simplicity, elegance and a great challenge!
+        </h4>
         <h2 className="about_backdrop_text">About me</h2>
       </div>
     </div>
