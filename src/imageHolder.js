06b90cbf5a0536b01c387879902d7be82ed77653
unknown <mvaluh@gmail.com>
2021-11-03 17:47:17 +0100

    Finished The Layout of Projects Page

--- src/imageHolder.js
+++ src/imageHolder.js
@@ -4,19 +4,19 @@ import mrP from "./images/movie_router_phone.png";
 import mrT from "./images/movie_router_tablet.png";
 
 // Expense Tracker
-import etD from "./images/movie_router_desktop.png";
-import etP from "./images/movie_router_phone.png";
-import etT from "./images/movie_router_tablet.png";
+import etD from "./images/expense_tracker_desktop.png";
+import etP from "./images/expense_tracker_phone.png";
+import etT from "./images/expense_tracker_tablet.png";
 
 // TodoList
-import tdD from "./images/movie_router_desktop.png";
-import tdP from "./images/movie_router_phone.png";
-import tdT from "./images/movie_router_tablet.png";
+import tdD from "./images/todo_list_desktop.png";
+import tdP from "./images/todo_list_tablet.png";
+import tdT from "./images/todo_list_tablet.png";
 
 // SeatBooking
-import sbD from "./images/movie_router_desktop.png";
-import sbP from "./images/movie_router_phone.png";
-import sbT from "./images/movie_router_tablet.png";
+import sbD from "./images/seat_booking_desktop.png";
+import sbP from "./images/seat_booking_phone.png";
+import sbT from "./images/seat_booking_tablet.png";
 
 export default {
   movieRouter: { mrD, mrP, mrT },
