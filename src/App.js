869cecf7606325b23ee1f420eb86077ffc4ae495
unknown <mvaluh@gmail.com>
2021-11-08 10:09:33 +0100

    Possible Fix #10

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
 import "./App.scss";
 import Home from "./components/Home/Home";
 import { Switch, Route } from "react-router-dom";
-import { useLocation } from "react-router";
 import Skills from "./components/Skills/Skills";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
-import { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
 import { AnimatePresence } from "framer-motion";
 
 function App() {
-  const location = useLocation();
-  const [page, setPage] = useState("");
-
-  useEffect(() => {
-    pageHandler(location.pathname);
-  }, [location]);
-
-  const pageHandler = (path) => {
-    switch (path) {
-      case "":
-        setPage("");
-        break;
-
-      case "about":
-        setPage("");
-        break;
-
-      case "skills":
-        setPage("01");
-        break;
-
-      case "projects":
-        setPage("02");
-        break;
-
-      case "contact":
-        setPage("03");
-        break;
-
-      default:
-        break;
-    }
-  };
-
   return (
     <div className="overlay">
       <AnimatePresence exitBeforeEnter>
